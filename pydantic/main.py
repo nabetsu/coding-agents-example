44411f11c554f54b494959db06907f95b5577194
nabetsu <panyoriokome@gmail.com>
2025-12-30 12:34:42 +0900

    setup pydantic-ai

--- pydantic/main.py
+++ pydantic/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from pydantic!")
+import os
+from pydantic_ai import Agent
+from pydantic_ai.models.openai import OpenAIChatModel
+from pydantic_ai.providers.openai import OpenAIProvider
+from dotenv import load_dotenv
 
+load_dotenv()
+
+openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+model = OpenAIChatModel("gpt-4o", provider=OpenAIProvider(api_key=openai_api_key))
+agent = Agent(
+    # instructions=instructions,
+    model=model,
+)
 
 if __name__ == "__main__":
-    main()
+  agent.to_cli_sync()
